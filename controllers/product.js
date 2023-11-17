0c8430d6800d025ff9c17f3ab7b62c7b45a37a83
ehoneahobed <ehoneahobed@hotmail.com>
2023-11-16 20:33:03 -0500

    Update get product by vendor feature

--- controllers/product.js
+++ controllers/product.js
@@ -355,7 +355,7 @@ exports.getApprovedProducts = async (req, res) => {
 // get all products from a given vendor
 exports.getProductsByVendor = async (req, res) => {
     try {
-        const products = await Product.find({ "vendor._id": req.params.vendorId });
+        const products = await Product.find({ "vendor": req.params.vendorId });
         if (!products) {
             return res.status(404).send({
                 message: "No products found for vendor with id " + req.params.vendorId
